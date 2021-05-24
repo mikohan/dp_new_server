24401e0c4996b73a055ccf34c3d7047d8988a8ef
mikohan <angara99@gmail.com>
2021-05-24 17:28:18 +0300

    Migration for emails blacklist added

--- dp/accounts/admin.py
+++ dp/accounts/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Profile
+from .models import BlackListEmail, Profile
 
 
 admin.site.register(Profile)
+
+admin.site.register(BlackListEmail)
 
 # Register your models here.
--- dp/accounts/models.py
+++ dp/accounts/models.py
@@ -3,20 +3,33 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.CharField(max_length=20, blank=True)
     address = models.CharField(max_length=500, blank=True)
-    country = models.CharField(max_length=255, default='Россия', null=True, blank=True)
+    country = models.CharField(max_length=255, default="Россия", null=True, blank=True)
 
     def __str__(self):
         return self.user.username
+
+
+class BlackListEmail(models.Model):
+    email = models.EmailField(max_length=255)
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = "Забанен"
+        verbose_name_plural = "Забанены"
 
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
